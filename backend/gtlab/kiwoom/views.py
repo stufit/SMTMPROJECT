8c42b21e60d818ace9c9f89c93f39205cbd9320d
stufit <rhksdud230@dankook.ac.kr>
2021-10-26 23:39:27 +0900

    메인페이지(로그인부분)수정중

--- backend/gtlab/kiwoom/views.py
+++ backend/gtlab/kiwoom/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def homelist(request):
+def mainpage(request):
     return render(request, 'kiwoom/mainpage.html')
+    '''
+    if request.method == 'GET':
+        return render(request, 'kiwoom/mainpage.html')
+    
+    elif request.method == 'POST':
+        username = request.POST.get('아이디',None)
+        password = request.POST.get('비밀번호',None)
+
+        res_data={}
+    ''' 
