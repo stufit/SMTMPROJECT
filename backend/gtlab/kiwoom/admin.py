6cf594105343fd85c149ba4f16cc6208f1141c0f
stufit <rhksdud230@dankook.ac.kr>
2021-10-26 23:38:41 +0900

    회원가입맴버추가

--- backend/gtlab/kiwoom/admin.py
+++ backend/gtlab/kiwoom/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import textboard
+from .models import textboard, MemberInfo
 # Register your models here.
 
 admin.site.register(textboard)
+class MemberInfoAdmin(admin.ModelAdmin):
+    list_display = ('username','email','password','created_at','updated_at')
+admin.site.register(MemberInfo,MemberInfoAdmin)
--- backend/gtlab/kiwoom/models.py
+++ backend/gtlab/kiwoom/models.py
@@ -4,6 +4,8 @@ from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+
+# 연습삼아 만들어봄
 class textboard(models.Model):
     
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -19,3 +21,20 @@ class textboard(models.Model):
 
     def __str__(self):
         return self.title
+
+# 관리자 페이지에서 유저들의 정보를 저장하기 위해 모델 선언
+class MemberInfo(models.Model):
+    username    = models.CharField(max_length=100, verbose_name='유저ID')
+    email       = models.EmailField(max_length=100, verbose_name='유저이메일')
+    password    = models.CharField(max_length=100, verbose_name='유저비밀번호')
+    created_at  = models.DateTimeField(auto_now_add=True, verbose_name='가입날짜')
+    updated_at  = models.DateTimeField(auto_now=True, verbose_name='마지막수정일')
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        db_table             = 'MemberInfo'
+        verbose_name         = '게시판 맴버'
+        verbose_name_plural  = '게시판맴버'
+
